d0f6f992823ee65ee14646c30e0b6ce34b63e976
Jayson Mourier <jayson.mourier@proton.me>
2025-04-11 02:12:22 +0200

    ensure extern 'C' compatibility for C++

--- array/array.h
+++ array/array.h
@@ -14,57 +14,65 @@
 #define ARRAY_ERR_NULL -4
 #define ARRAY_ERR_INTERNAL -5
 
-typedef struct {
-	size_t size;
-	size_t capacity;
-	void** data;
-} Array;
+#ifdef "__cplusplus"
+extern "C" {
+#endif
 
-/*
-* Initializes a new empty array
-*
-* @param array Pointer to the array structure
-* @param initial_capacity Initial capacity of the array
-* @return ARRAY_SUCCESS, ARRAY_ERR_INVALID_CAP or ARRAY_ERR_MEMALLOC
-*/
-int array_init(Array* array, size_t initial_capacity);
+	typedef struct {
+		size_t size;
+		size_t capacity;
+		void** data;
+	} Array;
 
-/*
-* Destroys an array
-*
-* @param array Pointer to the array structure
-* @param free_fn Pointer to the function used to free the data
-*/
-void array_destroy(Array* array, void (*free_fn)(void*));
+	/*
+	* Initializes a new empty array
+	*
+	* @param array Pointer to the array structure
+	* @param initial_capacity Initial capacity of the array
+	* @return ARRAY_SUCCESS, ARRAY_ERR_INVALID_CAP or ARRAY_ERR_MEMALLOC
+	*/
+	int array_init(Array* array, size_t initial_capacity);
 
-/*
-* Pushes the element at the end of the array
-*
-* @param array Pointer to the array structure
-* @param element Pointer to the element to push at the end of the array
-* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_INTERNAL
-*/
-int array_add(Array* array, void* element);
+	/*
+	* Destroys an array
+	*
+	* @param array Pointer to the array structure
+	* @param free_fn Pointer to the function used to free the data
+	*/
+	void array_destroy(Array* array, void (*free_fn)(void*));
 
-/*
-* Gets the element from the array at the given index
-*
-* @param array Pointer to the array structure
-* @param index Index of the element
-* @param element Output pointer where the retrieved element will be stored
-* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_OUT_OF_BOUNDS
-*/
-int array_get(Array* array, unsigned index, void** element);
+	/*
+	* Pushes the element at the end of the array
+	*
+	* @param array Pointer to the array structure
+	* @param element Pointer to the element to push at the end of the array
+	* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_INTERNAL
+	*/
+	int array_add(Array* array, void* element);
 
-/*
-* Sets the element from array at the given index
-*
-* @param array Pointer to the array structure
-* @param index Index at which to set the new element
-* @param new_element Pointer to the new element
-* @param free_fn Function used to free the old element at the given index
-* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_OUT_OF_BOUNDS
-*/
-int array_set(Array* array, unsigned index, void* new_element, void (*free_fn)(void*));
+	/*
+	* Gets the element from the array at the given index
+	*
+	* @param array Pointer to the array structure
+	* @param index Index of the element
+	* @param element Output pointer where the retrieved element will be stored
+	* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_OUT_OF_BOUNDS
+	*/
+	int array_get(Array* array, unsigned index, void** element);
+
+	/*
+	* Sets the element from array at the given index
+	*
+	* @param array Pointer to the array structure
+	* @param index Index at which to set the new element
+	* @param new_element Pointer to the new element
+	* @param free_fn Function used to free the old element at the given index
+	* @return ARRAY_SUCCESS, ARRAY_ERR_NULL or ARRAY_ERR_OUT_OF_BOUNDS
+	*/
+	int array_set(Array* array, unsigned index, void* new_element, void (*free_fn)(void*));
+
+#ifdef "__cplusplus"
+}
+#endif
 
 #endif // H_ARRAY
